3095eafb829009d1a2debe8b7e0fedb93c93ef19
atravieso <angeltraviesoc@gmail.com>
2023-04-24 01:08:05 -0400

    Atributos de una clase

--- 03.php
+++ 03.php
@@ -1,2 +1,31 @@
 <?php
 include 'includes/header.php';
+
+// Atributos de una clase
+class Empleado {
+
+    public $nombre;
+    public $apellido;
+    public $departamento;
+    public $email;
+    public $codigo;
+
+}
+
+$empleado = new Empleado;
+
+$empleado->nombre = 'Angel';
+$empleado->apellido = 'Travieso';
+
+echo '<pre>';
+var_dump($empleado);
+echo '</pre>';
+
+$empleado2 = new Empleado;
+
+$empleado2->nombre = 'Jose';
+$empleado2->apellido = 'Perez';
+
+echo '<pre>';
+var_dump($empleado2);
+echo '</pre>';
